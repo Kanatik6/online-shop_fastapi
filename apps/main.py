2e65d887dc70b2a6bb7ca6ef37310be1b6b610dd
Your Name <you@example.com>
2022-03-08 15:36:08 +0600

    final

--- apps/main.py
+++ apps/main.py
@@ -5,8 +5,16 @@ from products.routers import router as router_product
 
 from users.routers import router as router_user
 
-Base.metadata.create_all(bind=engine)
+
+def create_db():
+    Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="Shop")
+
+@app.on_event("startup")
+def on_startup():
+    create_db()
+
 
 JWT_SECRET = config('secret')
 JWT_ALGORITHM = config('algorithm')
--- apps/products/routers.py
+++ apps/products/routers.py
@@ -15,45 +15,45 @@ async def get_products(db: Session = Depends(get_db)):
 
 
 @router.get('/{id}/', response_model=schemas.ReturnProduct, tags=['products'])
-async def get_products(*, db: Session = Depends(get_db), id: int):
+async def get_product(*, db: Session = Depends(get_db), id: int):
     return crud.get_product(db=db, id=id)
 
 
-@router.post('/', response_model=schemas.ReturnProduct, tags=['products'])
-async def get_products(*, db: Session = Depends(get_db), product: schemas.Product):
+@router.post('/',dependencies=[Depends(JWTBearer())], response_model=schemas.ReturnProduct, tags=['products'])
+async def create_products(*, db: Session = Depends(get_db), product: schemas.Product):
     return crud.create_product(db=db, product=product)
 
 
-@router.put('/{id}/', response_model=schemas.ReturnProduct, tags=['products'])
-async def get_products(*, db: Session = Depends(get_db), product: schemas.Product, id: int):
+@router.put('/{id}/',dependencies=[Depends(JWTBearer())], response_model=schemas.ReturnProduct, tags=['products'])
+async def put_products(*, db: Session = Depends(get_db), product: schemas.Product, id: int):
     return crud.put_product(db=db, product=product, id=id)
 
 
-@router.patch('/{id}/', response_model=schemas.ReturnProduct, tags=['products'])
-async def get_products(*, db: Session = Depends(get_db), product: schemas.PatchProduct, id: int):
+@router.patch('/{id}/', dependencies=[Depends(JWTBearer())],response_model=schemas.ReturnProduct, tags=['products'])
+async def patch_products(*, db: Session = Depends(get_db), product: schemas.PatchProduct, id: int):
     return crud.patch_product(db=db, product=product, id=id)
 
 
-@router.delete('/{id}/', response_model=schemas.ReturnProduct, tags=['products'])
-async def get_products(*, db: Session = Depends(get_db), id: int):
+@router.delete('/{id}/', dependencies=[Depends(JWTBearer())],response_model=schemas.ReturnProduct, tags=['products'])
+async def delete_products(*, db: Session = Depends(get_db), id: int):
     return crud.delete_product(db=db, id=id)
 
 
 @router.post('/{id}/add/', response_model=schemas.ReturnCart, dependencies=[Depends(JWTBearer())], tags=['cart'])
-async def get_products(*, db: Session = Depends(get_db), cart_id: int = Depends(get_current_user), product: schemas.SetProduct):
+async def add_products(*, db: Session = Depends(get_db), cart_id: int = Depends(get_current_user), product: schemas.SetProduct):
     return crud.add_products(db=db, cart_id=cart_id, product=product)
 
 
 @router.post('/{id}/remove/', response_model=schemas.ReturnCart, dependencies=[Depends(JWTBearer())], tags=['cart'])
-async def get_products(*, db: Session = Depends(get_db), cart_id: int = Depends(get_current_user), product: schemas.SetProduct):
+async def remove_products(*, db: Session = Depends(get_db), cart_id: int = Depends(get_current_user), product: schemas.SetProduct):
     return crud.remove_products(db=db, cart_id=cart_id, product=product)
 
 
 @router.post('/{id}/buy/', response_model=schemas.ReturnCart, dependencies=[Depends(JWTBearer())], tags=['cart'])
-async def get_products(*, db: Session = Depends(get_db), cart_id: int = Depends(get_current_user)):
+async def buy_products(*, db: Session = Depends(get_db), cart_id: int = Depends(get_current_user)):
     return crud.remove_all_products(db=db, cart_id=cart_id)
 
 
 @router.get('/cart/{id}/', response_model=schemas.ReturnCart, tags=['cart'])
-async def get_products(*, db: Session = Depends(get_db), id: int):
+async def get_cart(*, db: Session = Depends(get_db), id: int):
     return crud.get_cart(db=db, id=id)
